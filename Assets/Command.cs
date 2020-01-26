15a0302a9c44bc307312cadb48b5918705566de9
James Donlan <jacketlizard@live.com>
2020-01-26 02:47:38 -0800

    controls now work through command and inputhandler

--- Assets/Command.cs
+++ Assets/Command.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Command{
+public abstract class Command
+{
     public abstract void Execute(Animator anim);
 }
 
 public class PerformJump: Command {
-    public override void Execute(Animator anim) {
+    public override void Execute(Animator anim) 
+    {
         anim.SetTrigger("isJumping");
     }
 }
 
-public class DoNothing: Command {
+public class PerformKick : Command {
+    public override void Execute(Animator anim) {
+        anim.SetTrigger("isKicking");
+    }
+}
+
+public class PerformPunch : Command {
+    public override void Execute(Animator anim) {
+        anim.SetTrigger("isPunching");
+    }
+}
+
+public class DoNothing: Command 
+{
     public override void Execute(Animator anim) {
     
     }
---
+++ Assets/InputHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public GameObject actor;
+    Animator anim;
+    Command keyQ, keyW, keyE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyQ = new PerformJump();
+        keyW = new PerformKick();
+        keyE = new PerformPunch();
+        anim = actor.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            keyQ.Execute(anim);
+        } else if (Input.GetKeyDown(KeyCode.W)) {
+            keyW.Execute(anim);
+        } else if (Input.GetKeyDown(KeyCode.E)) {
+            keyE.Execute(anim);
+        }
+    }
+}
